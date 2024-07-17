80bc0c4efa9565312aa97aaf2c0e2c2a92d933d7
psb <prabesh@apple.com>
2024-07-17 19:35:00 +0545

    game runs smoothly 1st stage completed for this project bare minimum is done

--- Bounce.py
+++ Bounce.py
@@ -1,20 +1,33 @@
 import pygame   #importing pygame library
 pygame.init()   #initializing pygame
 
-surface = pygame.display.set_mode((1080, 720), pygame.RESIZABLE)     #setting the size of the game window and also making it resizable
+from pygame import mixer
+mixer.init()
+
+import random
+
+surface = pygame.display.set_mode((360, 720), pygame.RESIZABLE)     #setting the size of the game window and also making it resizable
 
 pygame.display.set_caption("Bounce")     #Setting the title of our game window
 Icon = pygame.image.load("/Users/prabesh/Documents/Github/Bounce/app_media/Icon.png")      #Loading image first before setting it
 pygame.display.set_icon(Icon)   #Setting an icon of the game window
 
-vel = 10
-x = 540
-y = 670
+rocketsize = 50
+vel = 15
+rocketx = 180
+rockety = 670
+rockethealth = 100
 rocket_img = pygame.image.load("/Users/prabesh/Documents/Github/Bounce/app_media/Rocket.png")
-rocket_img = pygame.transform.scale(rocket_img,(50,50))
+rocket_img = pygame.transform.scale(rocket_img,(rocketsize,rocketsize))
 
 monster_img = pygame.image.load("/Users/prabesh/Documents/Github/Bounce/app_media/Space Monster.png")
-monster_img = pygame.transform.scale(monster_img, (1000,200))
+monster_img = pygame.transform.scale(monster_img, (250,170))
+
+bulletsize = 10
+bulletx = random.randint(0, 360-bulletsize)
+bullety = 150
+bullet_img = pygame.image.load("/Users/prabesh/Documents/Github/Bounce/app_media/Bullet.png")
+bullet_img = pygame.transform.scale(bullet_img, (bulletsize,bulletsize))
 
 running = True
 while running:  #keep game running till running is true
@@ -27,20 +40,33 @@ while running:  #keep game running till running is true
         keys = pygame.key.get_pressed() 
       
         # if left arrow key is pressed 
-        if keys[pygame.K_LEFT] and x>50: 
+        if keys[pygame.K_LEFT] and rocketx>0: 
             
             # decrement in x co-ordinate 
-            x -= vel 
+            rocketx -= vel 
             
         # if right arrow key is pressed 
-        if keys[pygame.K_RIGHT] and x<1080-50: 
+        if keys[pygame.K_RIGHT] and rocketx<360-50: 
             
             # increment in x co-ordinate 
-            x += vel 
+            rocketx += vel 
 
+    bullety = bullety + 0.7
     surface.fill((0,0,0))
 
-    surface.blit(monster_img, (50,-50))
-    surface.blit(rocket_img, (x,y))
+    surface.blit(monster_img, (50,0))
+    surface.blit(rocket_img, (rocketx,rockety))
+    surface.blit(bullet_img, (bulletx, bullety)) 
+    if rockety<(bullety+bulletsize):
+        bullety = 720+0.9
+        if (((rocketx<(bulletx+bulletsize))and ((rocketx+rocketsize)>(bulletx+bulletsize)))or (((rocketx+rocketsize)>bulletx)and((rocketx+rocketsize)<(bulletx+bulletsize)))or ((rocketx<(bulletx)) and ((rocketx+rocketsize)>(bulletx+bulletsize)))):
+            rockethealth = rockethealth - 50
+
+    if bullety>720:
+        bullety = 150
+        bulletx = random.randint(0, 360-bulletsize)
     
+    if rockethealth<=0:
+        running = False
+
     pygame.display.update()     #Update the GUI
